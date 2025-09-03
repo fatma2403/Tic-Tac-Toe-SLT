0c8274d92086a869b7623aa70be07773558ede81
Fatma Abouzaid <wi24b035@technikum-wien.at>
2025-09-03 19:36:00 +0200

    Added Logic to Board class

--- src/main/java/org/tictactoe/Board.java
+++ src/main/java/org/tictactoe/Board.java
@@ -1,9 +1,14 @@
 package org.tictactoe;
 
+import java.util.Arrays;
+
 public class Board {
     private char[][] board;
     public Board() {
         board = new char[3][3];
+        for (char[] chars : board) {
+            Arrays.fill(chars, ' ');
+        }
     }
     public boolean isCellEmpty(int x, int y){
         return board[x][y] == ' ';
@@ -11,17 +16,29 @@ public class Board {
     public void place(int x, int y, char marker){
         board[x][y] = marker;
     }
-    public boolean isFull(){
-        return false;
+    public boolean isFull() {
+        return Arrays.stream(board)         // stream of rows
+                .flatMapToInt(row -> new String(row).chars())
+                .noneMatch(ch -> ch == ' ');
     }
     public void clear(){
+
         board = new char[3][3];
+        for (char[] chars : board) {
+            Arrays.fill(chars, ' ');
+        }
     }
     public void print(){
+        System.out.println("▁▁▁▁▁▁▁");
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
+                System.out.print("|");
                 System.out.print(board[i][j]);
+                if(j == 2){
+                    System.out.print("|\n");
+                }
             }
         }
+        System.out.println("▔▔▔▔▔▔▔");
     }
 }
